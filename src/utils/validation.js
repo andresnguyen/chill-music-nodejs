e636da0131d4a36c0147e5289cc5b1cdaed5b8da
Coders.Mickey <thuongnguyen.it78@gmail.com>
2021-04-26 21:08:22 +0700

    do Joi validation

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -2,8 +2,10 @@ const Joi = require('joi')
 
 export const loginValidation = (data) => {
     const schemaLogin = Joi.object({
-        email: Joi.string().email(),
-        password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,30}$')),
+        email: Joi.string().email().required(),
+        password: Joi.string()
+            .pattern(new RegExp('^[a-zA-Z0-9]{6,30}$'))
+            .required(),
     })
 
     return schemaLogin.validate(data)
@@ -11,12 +13,14 @@ export const loginValidation = (data) => {
 
 export const registerValidation = (data) => {
     const schemaRegister = Joi.object({
-        email: Joi.string().email(),
-        fullname: Joi.string().email(),
-        dayOfBirth: Joi.string().min(8).max(8).require(),
-        gender: Joi.string().email(),
-        password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-        rePassword: Joi.ref('password'),
+        email: Joi.string().email().required(),
+        fullname: Joi.string().required(),
+        dayOfBirth: Joi.string().required(),
+        gender: Joi.string().required(),
+        password: Joi.string()
+            .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+            .required(),
+        rePassword: Joi.ref('password').required(),
     })
 
     return schemaRegister.validate(data)
@@ -24,29 +28,29 @@ export const registerValidation = (data) => {
 
 export const songValidation = (data) => {
     const schemaSong = Joi.object({
-        name: Joi.string().email(),
-        path: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-        image: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-        genre: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-        artists: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+        name: Joi.string().required(),
+        path: Joi.string().required(),
+        image: Joi.string().required(),
+        genre: Joi.string().required(),
+        artists: Joi.array().required(),
     })
 
     return schemaSong.validate(data)
 }
 
 export const albumValidation = (data) => {
-    const schemaSong = Joi.object({
-        name: Joi.string().email(),
-        image: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-        artists: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+    const schemaAlbum = Joi.object({
+        name: Joi.string().required(),
+        image: Joi.string().required(),
+        artists: Joi.string().array().required(),
     })
 
     return schemaAlbum.validate(data)
 }
 
 export const playlistValidation = (data) => {
-    const schemaSong = Joi.object({
-        name: Joi.string().email(),
+    const schemaPlaylist = Joi.object({
+        name: Joi.string().required(),
     })
 
     return schemaPlaylist.validate(data)
