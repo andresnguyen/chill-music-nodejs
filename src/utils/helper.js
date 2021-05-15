7d391d15748296d629473f920f0904997167b0a0
Coders.Mickey <thuongnguyen.it78@gmail.com>
2021-05-15 12:55:59 +0700

    fix: bcrypt error

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,10 +1,10 @@
-const bcrypt = require('bcrypt')
+import bcrypt from 'bcryptjs'
 import path from 'path'
 import rfs from 'rotating-file-stream'
 
 export const accessLogStream = rfs('access.log', {
     interval: '1d',
-    path: path.resolve(__dirname, '..', '..', '..', 'log'),
+    path: path.resolve(__dirname, '..', 'log'),
 })
 
 export const encodePassword = async (password) => {
